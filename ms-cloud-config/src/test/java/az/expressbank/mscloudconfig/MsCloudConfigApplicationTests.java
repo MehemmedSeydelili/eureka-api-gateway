439b7e11c01d42766c69dc3bc0f95c1df25a6385
m.saydalili <m.saydalili@expressbank.az>
2023-12-08 11:14:11 +0400

    Initial commit

---
+++ ms-cloud-config/src/test/java/az/expressbank/mscloudconfig/MsCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package az.expressbank.mscloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ms-user/src/main/java/az/expressbank/msuser/controller/UserController.java
@@ -0,0 +1,23 @@
+package az.expressbank.msuser.controller;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final Environment environment;
+
+    public UserController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping
+    public String sayUser(){
+        return "Port number: " + environment.getProperty("local.server.port");
+    }
+
+}
