4faf84e3a3c46559abf4ec41c9ad5e033ee3e920
m.saydalili <m.saydalili@expressbank.az>
2023-12-13 18:02:38 +0400

    Initial commit

--- ms-security/src/main/java/az/expressbank/mssecurity/MsSecurityApplication.java
+++ ms-security/src/main/java/az/expressbank/mssecurity/MsSecurityApplication.java
@@ -1,13 +1,18 @@
 package az.expressbank.mssecurity;
 
+import az.expressbank.mssecurity.data.entity.UserInfo;
 import az.expressbank.mssecurity.data.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class MsSecurityApplication {
+public class MsSecurityApplication implements CommandLineRunner {
 
 
     private final UserRepository userRepository;
@@ -17,18 +22,13 @@ public class MsSecurityApplication {
         SpringApplication.run(MsSecurityApplication.class, args);
     }
 
-   /* @Override
+    @Override
     public void run(String... args) throws Exception {
-        Optional<Role> admin = userRepository.findByRole(Role.ADMIN);
-        if (admin.isEmpty()){
-            User user = new User();
-            user.setName("mahammad");
-            user.setSecondName("seydalili   ");
-            user.setEmail("mm@gmail.com");
-            user.setPassword(new BCryptPasswordEncoder().encode("mahammad1"));
-            user.setRole(Role.ADMIN);
 
+            UserInfo user = new UserInfo();
+            user.setUsername("mahammad");
+            user.setPassword(new BCryptPasswordEncoder().encode("mahammad"));
             userRepository.save(user);
-        }
-    }*/
+
+    }
 }
--- ms-security/src/main/java/az/expressbank/mssecurity/data/entity/UserInfo.java
+++ ms-security/src/main/java/az/expressbank/mssecurity/data/entity/UserInfo.java
@@ -24,7 +24,7 @@ import java.util.Set;
 public class UserInfo{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private long id;
     private String username;
